e7c5b9368167a0f5613242bb7f8fb8198064f2de
JonathanBrunoS <jonathan.gestao@gmail.com>
2025-07-09 12:58:25 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app.jsx';  // Importa o arquivo app.jsx do mesmo diretório (ou ajuste o caminho conforme)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
